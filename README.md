4393c55a2e52d06d01450a61561682c314589347
CrazyWillBear <crazywillbear@gmail.com>
2021-12-04 06:30:45 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# `yes` in Rust
+
+## Why
+I coded this to prove that simple programs can have simple code. If you look at the source code for GNU's version of `yes` ([link](https://github.com/coreutils/coreutils/blob/master/src/yes.c)), you can see that it is full of complicated and mostly useless code (not shitting on GNU, I have a lot of respect for the GNU project). For example, Suckless has also reprogrammed `yes` ([link](http://git.suckless.org/sbase/file/yes.c.html)), and their version is simple and bloat-free. The difference between mine and Suckless's, is that mine retains slightly more functionality (literally the version command and unknown option error).
+
+## Why Rust?
+I see Rust as the future of systems programming, and I feel that as the language becomes more optimized, it will eventually be faster than C. I want to code such tools in Rust to encourage others to do the same.
+
+## License
+Obviously this project is small and should be used as a resource for research, so I gave it the Unlicense.
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+/*
+  This software is licensed under the Unlicense. If a copy
+  of said license wasn't provided with the code, you can go
+  to https://unlicense.org/ to get one.
+
+  Written by CrazyWillBear (https://crazywillbear.github.io),
+  2021
+*/
+
+use std::env;
+
+// Software Version: '0.1.0'
+const VER: &str = "0.1.0";
+
+// usage function, sdf tring is formatted to represent text
+// output in terminal.
+fn usage() {
+  eprintln!("
+Usage: 'yes <input>...'
+  or: 'yes OPTION'\n
+Repeatedly prints input until killed.
+  - Prints 'y' if input isn't provided.\n
+Options:
+  --help\tdisplay help menu
+  --version\tdisplay version
+  ")
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+
+  // statement's default value is 'y'; will change later if
+  // necessary
+  let mut statement: String = "y".to_string();
+
+  // if there are more args than just the program, user inputed
+  // something, so assign statement whatever the input is
+  if args.len() > 1 {
+    // if user inputs '--help', run usage function and exit
+    if args[1] == "--help" {
+      usage();
+      return;
+    }
+    // if user inputs '--version', display version
+    if args[1] == "--version" {
+      eprintln!("\nVersion = {}\n", VER);
+      return;
+    }
+    // if the user inputs a '-<char>', and it failed the above
+    // checks, the user put an unknown option, so throw error
+    // and exit
+    if args[1].split("").collect::<Vec<&str>>()[1] == "-" && args[1] != "-" {
+      eprintln!("\nOption unknown '{}'\n\t- Use '--help' for more info\n", args[1]);
+      return;
+    }
+
+    // create final statement
+    statement = args[1..args.len()].join(" ");
+  }
+
+  // print statement forever (until killed)
+  loop { println!("{}", statement); }
+}
